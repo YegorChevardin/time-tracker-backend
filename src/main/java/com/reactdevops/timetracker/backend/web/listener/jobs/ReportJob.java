b3e6fd714ec2b3080edac4ab2bc42cfba8c3dbb3
Ivan <ivantumentsev@gmail.com>
2023-10-01 17:26:19 +0300

    Rebased from main

--- src/main/java/com/reactdevops/timetracker/backend/web/listener/jobs/ReportJob.java
+++ src/main/java/com/reactdevops/timetracker/backend/web/listener/jobs/ReportJob.java
@@ -1,5 +1,6 @@
 package com.reactdevops.timetracker.backend.web.listener.jobs;
 
+import com.reactdevops.timetracker.backend.service.bot.TelegramBot;
 import com.reactdevops.timetracker.backend.service.services.EmailService;
 import com.reactdevops.timetracker.backend.service.services.impl.EmailServiceImpl;
 import org.apache.logging.log4j.Level;
@@ -13,17 +14,17 @@ public class ReportJob implements Job {
   private static final Logger logger = LogManager.getLogger(ReportJob.class);
 
   private EmailService emailService = new EmailServiceImpl();
+  private TelegramBot bot = new TelegramBot();
 
   @Override
   public void execute(JobExecutionContext jobExecutionContext) {
     logger.log(Level.INFO, "Creating and sending reports...");
-
     try {
       emailService.sendEmail(MENTOR_EMAIL);
     } catch (Exception e) {
       logger.log(Level.ERROR, e.getMessage(), e);
       throw e;
     }
-    // todo paste telegram service action here
+    bot.sendReport();
   }
 }
